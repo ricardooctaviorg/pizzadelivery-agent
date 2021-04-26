f551a48ac0d7f6db142b78a4aa1d3e7f39b3deef
Ricardo Octavio Rojas Garcia <ricardooctaviorg@gmail.com>
2021-04-26 18:54:21 -0500

    add icon notifications

--- src/app/commons/components/header-menu/header-menu.component.ts
+++ src/app/commons/components/header-menu/header-menu.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+const ANIMATE_CURRENT           = "animate__wobble";
+const PATH_SOUND_CURRENT        = "../../../../assets/sounds/Information_Bell.ogg";
+const ELEMENT_OF_NOTIFICATION   = ".myNotification"
+
 @Component({
   selector: 'app-header-menu',
   templateUrl: './header-menu.component.html',
@@ -9,8 +13,43 @@ export class HeaderMenuComponent implements OnInit {
 
   @Input() titleMain:string;
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {}
+
+  onClick(){
+
+    this.animateCSS(ELEMENT_OF_NOTIFICATION,ANIMATE_CURRENT).then((message) => {
+      
+    });
+
+  }
+
+  animateCSS(element, animation): Promise<string> {
+    return new Promise(
+      (resolve, reject) => {
+        const animationName = animation;
+        const node = document.querySelector(element);
+        node.classList.add('animate__animated', animationName);
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+          event.stopPropagation();
+          node.classList.remove('animate__animated', animationName);
+          resolve('Animation ended');
+        }
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+        this.playAudio();
+      }
+    );
+  }
+
+  playAudio(){
+    let audio = new Audio();
+    audio.src = PATH_SOUND_CURRENT;
+    audio.load();
+    audio.play();
+  }
 
 }
